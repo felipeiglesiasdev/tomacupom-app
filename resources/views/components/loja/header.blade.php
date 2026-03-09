4df0a4540bc2217165c055eb2290dd3f3922c3a4
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2026-03-09 16:31:40 -0300

    ajuste cor do botão!

--- resources/views/components/loja/header.blade.php
+++ resources/views/components/loja/header.blade.php
@@ -13,7 +13,7 @@
             </p>
             <div class="flex flex-wrap mt-2 md:mt-0">
                 <a href="{{ $loja->url_base_afiliado }}" target="_blank" rel="nofollow" 
-                   class="group px-4 py-2.5 md:px-6 md:py-3 bg-[#fe4b09] text-white border border-[#fe4b09] hover:bg-white hover:text-[#fe4b09] hover:-translate-y-1 hover:shadow-lg no-underline rounded-md text-xs md:text-sm font-bold transition-all duration-300 flex items-center gap-2">
+                   class="group px-4 py-2.5 md:px-6 md:py-3 bg-[#ffffff] text-[#171717] hover:bg-[#fe4b09] hover:text-[#ffffff] hover:-translate-y-1 hover:shadow-lg no-underline rounded-md text-xs md:text-sm font-bold transition-all duration-300 flex items-center gap-2">
                     <span>Visitar Site Oficial</span>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
