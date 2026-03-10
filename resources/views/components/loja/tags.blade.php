f517a770bc0b979a02084f594459dfc15020ea24
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2026-03-10 11:28:16 -0300

    ajuste das tags de SEO

--- resources/views/components/loja/tags.blade.php
+++ resources/views/components/loja/tags.blade.php
@@ -7,5 +7,6 @@
 <meta property="og:url" content="https://www.tomacupom.com.br/cupons/{{$loja->slug}}">
 <meta property="og:title" content="{{ $loja->seo->title_seo }}">
 <meta property="og:description" content="{{ $loja->seo->description_seo }}">
+<meta property="og:image" content="{{ $loja->logo_image_link }}">
 <meta name="robots" content="index, follow">
 <link rel="canonical" href="https://www.tomacupom.com.br/cupons/{{$loja->slug}}">
